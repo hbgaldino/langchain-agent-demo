526d5b8a89f96c3b30dd26a63d4431cbcb6d236b
higor.galdino <higor.galdino@bayer.com>
2026-02-16 13:11:38 -0300

    example 2

--- src/ex002/main.py
+++ src/ex002/main.py
@@ -1,8 +1,35 @@
 from langchain.chat_models import init_chat_model
+from langchain_core.messages import SystemMessage, HumanMessage
 from rich import print
 
 llm = init_chat_model('ollama:qwen2')
 
-response = llm.invoke('Olá tudo bem?')
+system_message = SystemMessage("Você é um assistente útil e amigável.")
 
-print(response)
+human_message = HumanMessage("Olá meu nome é Higor")
+
+messages = [system_message, human_message]
+
+response = llm.invoke(messages)
+
+print(f"{'AI':-^80}")
+print(response.content)
+
+messages.append(response)
+
+while True:
+    user_input = input("Você: ")
+    human_message = HumanMessage(user_input)
+
+    if user_input.lower() in ['sair', 'exit', 'quit']:        
+        break
+
+    messages.append(human_message)
+
+    response = llm.invoke(messages)
+
+    print(f"{'AI':-^80}")
+    print(response.content)
+    print()
+
+    messages.append(response)
